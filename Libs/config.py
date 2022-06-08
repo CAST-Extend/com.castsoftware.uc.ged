8694297688bc0de4362423729969c74978d2b16c
Nevin Kaplan <n.kaplan@castsoftware.com>
2022-06-08 16:27:17 -0400

    configuration changes

--- Libs/config.py
+++ Libs/config.py
@@ -1,9 +1,12 @@
 """
     Read and validate configuartion json file
 """
+from logging import WARN
 from logger import Logger
 from json import load
 from json import JSONDecodeError
+
+from sqlalchemy import true
 
 
 __author__ = "Nevin Kaplan"
@@ -20,18 +23,38 @@ class Config(Logger):
             with open(config, 'rb') as config_file:
                 self.__config = load(config_file)
 
+            # AIP must always be active
+            self.__config['AIP']['active']=True
+
             for v in ['report_path','AIP','NEO4J','CSS','IMAGING']:
                 if v not in self.__config or len(self.__config[v]) == 0:
                     raise ValueError(f"Required field '{v}' is missing from config.json")
 
-            for v in ['NEO4J','CSS']:
+            for v in ['AIP','NEO4J','CSS','IMAGING']:
                 json = self.__config[v]
-                for p in ["host","port","user","password"]:
-                    if p not in json or len(p) == 0:
+                if 'active' not in json or json['active'] == True:
+                    self.__config[v]['active']=True
+                    valid = True
+                    if v in ['AIP']:
+                        for p in ["name","triplet_prefix","url","user","password"]:
+                            if p not in json or len(p) == 0:
+                                valid=False
+                                break
+                    elif v in ['NEO4J','CSS']:
+                        for p in ["host","port","user","password"]:
+                            if p not in json or len(p) == 0:
+                                valid=False
+                                break
+                    elif v in ['IMAGING']:
+                        for p in ["url","api_key"]:
+                            if p not in json or len(p) == 0:
+                                valid=False
+                                break
+                    if not valid:
                         raise ValueError(f"Required field '{p}' is missing from '{v}' in config.json")
-
-
-
+                else: 
+                    self.__config[v]['active']=False
+                    self.warning(f"{v} configuration is inactive")
 
         except JSONDecodeError as e:
             msg = str(e)
@@ -46,66 +69,82 @@ class Config(Logger):
             exit()
 
     @property
-    def aip_name(self):
-        return self.__config['aip_name']
-    
+    def AIP(self):
+        return self.__config['AIP']['active']
+
     @property
-    def aip_triplet_prefix(self):
-        return self.__config['aip_triplet_prefix']
-    
+    def NEO4J(self):
+        return self.__config['NEO4J']['active']
+
     @property
-    def aip_rest_url(self):
-        return self.__config['aip_rest_url']
-    
+    def CSS(self):
+        return self.__config['CSS']['active']
+
     @property
-    def aip_rest_user(self):
-        return self.__config['aip_rest_user']
-    
-    @property
-    def aip_rest_password(self):
-        return self.__config['aip_rest_password']
+    def IMAGING(self):
+        return self.__config['IMAGING']['active']
 
     @property
     def report_path(self):
         return self.__config['report_path']
 
     @property
+    def aip_name(self):
+        return self.__config['AIP']['name']
+    
+    @property
+    def aip_triplet_prefix(self):
+        return self.__config['AIP']['triplet_prefix']
+    
+    @property
+    def aip_rest_url(self):
+        return self.__config['AIP']['rest_url']
+    
+    @property
+    def aip_rest_user(self):
+        return self.__config['AIP']['rest_user']
+    
+    @property
+    def aip_rest_password(self):
+        return self.__config['AIP']['rest_password']
+
+    @property
     def neo4j_host(self):
-        return self.__config['neo4j_host']
+        return self.__config['NEO4J']['host']
 
     @property
     def neo4j_port(self):
-        return self.__config['neo4j_port']
+        return self.__config['NEO4J']['port']
 
     @property
     def neo4j_user(self):
-        return self.__config['neo4j_user']
+        return self.__config['NEO4J']['user']
     
     @property
     def neo4j_password(self):
-        return self.__config['neo4j_password']
+        return self.__config['NEO4J']['password']
     
     @property
     def css_host(self):
-        return self.__config['css_host']
+        return self.__config['CSS']['host']
 
     @property
     def css_port(self):
-        return self.__config['css_port']
+        return self.__config['CSS']['port']
 
     @property
     def css_user(self):
-        return self.__config['css_user']
+        return self.__config['CSS']['user']
     
     @property
     def css_password(self):
-        return self.__config['css_password']
+        return self.__config['CSS']['password']
     
     @property
     def imaging_rest_url(self):
-        return self.__config['imaging_rest_url']
+        return self.__config['IMAGING']['rest_url']
     
     @property
     def imaging_rest_api_key(self):
-        return self.__config['imaging_rest_api_key']
+        return self.__config['IMAGING']['api_key']
 
