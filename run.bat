9be090ce4e4b484ad181bc812b78d9dd7c7d5420
Hector Luis Rodriguez <hl.rodriguez@castsoftware.com>
2022-06-16 13:55:18 +0200

    Updated run.bat to use installed Python and external Libraries

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @ECHO OFF
 setlocal
-set PYTHONPATH=%cd%\Python;%cd%\Python\DLLs;%cd%\Python\lib;%cd%\Python\lib\site-packages;%cd%\ExtLibs
-Python\python.exe src\generateReports.py --config config.json --query query.json
+python.exe src\generateReports.py --config config.json --query query.json
 endlocal
