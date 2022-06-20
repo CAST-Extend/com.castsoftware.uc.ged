a9bcb8841d023f20cba06eebfcbc066d4f9e55c1
LKS <LKS@LKSLAP.corp.castsoftware.com>
2022-06-20 17:45:44 +0530

    The Argument correction to src\generateReports.py

--- runEmbeddedPythonDependencies.bat
+++ runEmbeddedPythonDependencies.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 setlocal
 set PYTHONPATH=%cd%\Python;%cd%\Python\DLLs;%cd%\Python\lib;%cd%\Python\lib\site-packages;%cd%\ExtLibs
-Python\python.exe generateReports.py --config config.json --query query.json
+Python\python.exe src\generateReports.py --config config.json --query query.json
 endlocal
